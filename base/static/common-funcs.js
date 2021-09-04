a2da6ca7c9ab6b4765acbe35f62b3f3728f8c796
Elizabeth Mattijsen <liz@wenzperl.nl>
2021-09-04 22:56:41 +0200

    Add some basic documentation to Javascript functions
    - also reorganize the order in which functions are defined
    - removed some dead code
    - improved naming of some functions
    - fix bug with Javascript code containing <.channel> references

--- base/static/common-funcs.js
+++ base/static/common-funcs.js
@@ -1,7 +1,27 @@
+// whether sidebars to be shown or not
+var $showLeftSide  = getCookie("showLeftSide");
+var $showRightSide = getCookie("showRightSide");
+
+// targets for gist of current channel
+var $currentChannel;
+var $gistTargets;
+
+// Filter settings
+var $filterMessagesByNick = getCookie("MessagesByNick");
+var $filterMessagesByText = getCookie("MessagesByText");
+var $filterExcludeByNick  = getCookie("ExcludeByNick");
+var $filterExcludeByText  = getCookie("ExcludeByText");
+var $showSystemMessages   = getCookie("SystemMessages");
+var $hideCommitMessages   = getCookie("CommitMessages");
+var $hideCameliaOutput    = getCookie("CameliaOutput");
+
+// Generic function for setting a cookie
 function setCookie(name, value) {
     document.cookie = name + "=" + value + ";path=/";
     return value;
 }
+
+// Generic function for getting a cookie
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -23,71 +43,12 @@ function getCookie(cname) {
     return "";
 }
 
-var $gistTargets = getTargets();
-
-var $filterMessagesByNick = getCookie("MessagesByNick");
-var $filterMessagesByText = getCookie("MessagesByText");
-var $filterExcludeByNick  = getCookie("ExcludeByNick")  // false;
-var $filterExcludeByText  = getCookie("ExcludeByText")  // false;
-var $showSystemMessages   = getCookie("SystemMessages") // false;
-var $hideCommitMessages   = getCookie("CommitMessages") // false;
-var $hideCameliaOutput    = getCookie("CameliaOutput")  // false;
-
-var $showLeftSide  = getCookie("showLeftSide")  // false;
-var $showRightSide = getCookie("showRightSide") // false;
-
-/* Set the width of the sidebar to 300px and the left margin of the page content to 300px */
-
-function setDisplay(id, state) {
-    document.getElementById(id).style.display = state ? "block" : "none";
-}
-function toggleLeftSide() {
-    $showLeftSide = setCookie('showLeftSide', !$showLeftSide);
-    setDisplay('left-sidebar', $showLeftSide);
-}
-function toggleRightSide() {
-    $showRightSide = setCookie('showRightSide', !$showRightSide);
-    setDisplay('right-sidebar', $showRightSide);
-}
-
-function submitSearchIfEnter() {
-    if (event.keyCode == 13 && okToSubmitSearch()) {
-        document.getElementById('Search').submit();
-    }
-}
-function submitSearchOnChannel(channel) {
-    document.getElementById('SearchChannel').value = channel;
-    document.getElementById('Search').submit();
-}
-function okToSubmitSearch() {
-    return document.getElementById('SearchChannel').value;
-}
-function checkChannel() {
-    if (okToSubmitSearch()) {
-        return true;
-    }
-    document.getElementById('SearchChannel').focus();
-    return false;
-}
-
+// Generic function to scroll to the bottom of the window
 function scrollToBottom() {
     window.scrollTo(0, document.body.scrollHeight);
 }
 
-function hideChannelsWithoutTargets() {
-    let items = document.querySelectorAll('li[id]');
-    for (let i = 0; items.length > i; i++) {
-        let item = items[i];
-        if (!getTargets(item.getAttribute('id'))) {
-            item.classList.add('is-hidden');
-        }
-    }
-}
-
-function getTargets(channel = '<.channel>') {
-    return getCookie(channel + "Targets");
-}
-
+// Generic function to set/unset "is-primary" class of an element
 function setPrimaryByElement(element, primary) {
     if (primary) {
         element.classList.remove('is-primary');
@@ -97,13 +58,7 @@ function setPrimaryByElement(element, primary) {
     }
 }
 
-function setButtons(name, state) {
-    let buttons = document.querySelectorAll('button[id=' + name + ']');
-    for (let i = 0; buttons.length > i; i++) {
-        setPrimaryByElement(buttons[i], state);
-    }
-}
-
+// Generic function to set/unset "is-hidden" class on an element
 function setVisibilityByElement(element, visible) {
     if (visible) {
         element.classList.remove('is-hidden');
@@ -113,6 +68,7 @@ function setVisibilityByElement(element, visible) {
     }
 }
 
+// Generic function to set/unset "is-hidden" class on elements by class name
 function setVisibilityByName(name, visible) {
     let rows = document.getElementsByClassName(name);
     for (let i = 0; rows.length > i; i++) {
@@ -120,6 +76,20 @@ function setVisibilityByName(name, visible) {
     }
 }
 
+// Generic function to set/unset "is-primary" class for buttons by id
+function setButtons(name, state) {
+    let buttons = document.querySelectorAll('button[id=' + name + ']');
+    for (let i = 0; buttons.length > i; i++) {
+        setPrimaryByElement(buttons[i], state);
+    }
+}
+
+// Generic function to set display state of an element by ID
+function setDisplayById(id, state) {
+    document.getElementById(id).style.display = state ? "block" : "none";
+}
+
+// Generic function to set state of checkboxes by id
 function setCheckboxes(name, state) {
     let boxes = document.querySelectorAll('input[id=' + name + ']');
     for (let i = 0; boxes.length > i; i++) {
@@ -127,6 +97,198 @@ function setCheckboxes(name, state) {
     }
 }
 
+// Toggle visibility of left sidebar
+function toggleLeftSidebar() {
+    $showLeftSide = setCookie('showLeftSide', !$showLeftSide);
+    setDisplayById('left-sidebar', $showLeftSide);
+}
+
+// Toggle visibility of right sidebar
+function toggleRightSidebar() {
+    $showRightSide = setCookie('showRightSide', !$showRightSide);
+    setDisplayById('right-sidebar', $showRightSide);
+}
+
+// Submit search if Enter was pressed and all requirements are met
+function submitSearchIfEnter() {
+    if (event.keyCode == 13 && okToSubmitSearch()) {
+        document.getElementById('Search').submit();
+    }
+}
+
+// Submit search with current parameters on given channel
+function submitSearchOnChannel(channel) {
+    document.getElementById('SearchChannel').value = channel;
+    document.getElementById('Search').submit();
+}
+
+// Return whether it is ok to submit a search request
+function okToSubmitSearch() {
+    return document.getElementById('SearchChannel').value;
+}
+
+// Check search form, highlight part(s) that are not ok if not ok
+function checkSearchParameters() {
+    if (okToSubmitSearch()) {
+        return true;
+    }
+    document.getElementById('SearchChannel').focus();
+    return false;
+}
+
+// Get the gist targets for a channel
+function getGistTargets(channel = $currentChannel) {
+    return getCookie(channel + "Targets");
+}
+
+// Set gist targets in cookie and set the Gist link with those targets
+function setGistTargets(targets = $gistTargets) {
+    $gistTargets = setCookie($currentChannel + 'Targets', targets);
+    document.getElementById('Gist').href =
+      '/' + $currentChannel + '/gist.html?' + targets;
+}
+
+// Get the element that has a "target" attribute in the given element
+function elementOfTarget(element, target) {
+    return document.querySelector(element + '[target="' + target + '"]');
+}
+
+// Reset all gist targets for the current channel, update messages accordingly
+function clearGistTargets() {
+    setGistTargets("");
+    markSelected();
+}
+
+// Add visible messages' targets to the gist targets of current channel
+function addVisibleTargets() {
+    let newTargets = $gistTargets.split(',');;
+    let haystack   = $gistTargets + ',';
+    let trs = document.querySelectorAll('tr[target]');
+    for (let i = 0; trs.length > i; i++) {
+        let tr = trs[i];
+        if (!tr.classList.contains('is-hidden')) {
+            let target = tr.getAttribute('target');
+            if (!haystack.includes(target + ',')) {
+                setTargetSelectionState(target, true);
+                newTargets.push(target);
+            }
+        }
+    }
+    setGistTargets(newTargets.sort().join(','));
+}
+
+// Hide channels from channel pulldown that don't have gist targets
+function hideChannelsWithoutGistTargets() {
+    let items = document.querySelectorAll('li[id]');
+    for (let i = 0; items.length > i; i++) {
+        let item = items[i];
+        if (!getGistTargets(item.getAttribute('id'))) {
+            item.classList.add('is-hidden');
+        }
+    }
+}
+
+// Remove targets of visible messages from the gist targets of current channel
+function removeVisibleTargets() {
+    let currentTargets = $gistTargets.split(",");
+    let newTargets = [];
+    for (let i = 0; currentTargets.length > i; i++) {
+        let target = currentTargets[i];
+        let tr     = elementOfTarget('tr', target);
+        if (tr) {
+            if (tr.classList.contains('is-hidden')) {
+                newTargets.push(target);
+            }
+            else {
+                setTargetSelectionState(target, false);
+            }
+        }
+    }
+    setGistTargets(newTargets.sort().join(','));
+}
+
+// Set gist selection state of given target, update highlight and pulldown menu
+function setTargetSelectionState(target, on) {
+    let tr = elementOfTarget('tr', target);
+    if (tr) {
+        if (on) {
+            tr.classList.add('special-selected');
+        }
+        else {
+            tr.classList.remove('special-selected');
+        }
+    }
+    let option = elementOfTarget('a', target);
+    if (option) {
+        if (on) {
+            option.classList.add('is-hidden');
+            option.nextSibling.classList.remove('is-hidden');
+        }
+        else {
+            option.classList.remove('is-hidden');
+            option.nextSibling.classList.add('is-hidden');
+        }
+    }
+}
+
+// Remove target of given link, updates status, visibility and cookie
+function removeTargetFromGist(link) {
+    let target = link.previousSibling.getAttribute('target');
+    let currentTargets = $gistTargets.split(',');
+    let newTargets = [];
+    let remove = false;
+    for (let i = 0; currentTargets.length > i; i++) {
+        let current = currentTargets[i];
+        if (current) {
+            if (current == target) {
+                remove = true;
+            }
+            else {
+                newTargets.push(current);
+            }
+        }
+    }
+    if (remove) {
+        setTargetSelectionState(target, false);
+    }
+    setGistTargets(newTargets.sort().join(','));
+}
+
+// Add target of given link, updates status, visibility and cookie
+function addTargetToGist(link) {
+    let target = link.getAttribute('target');
+    let currentTargets = $gistTargets.split(',');
+    let newTargets = [];
+    let add = true;
+    for (let i = 0; currentTargets.length > i; i++) {
+        let current = currentTargets[i];
+        if (current) {
+            if (current == target) {
+                add = false;
+            }
+            else {
+                newTargets.push(current);
+            }
+        }
+    }
+    if (add) {
+        setTargetSelectionState(target, true);
+        newTargets.push(target);
+    }
+    setGistTargets(newTargets.sort().join(','));
+}
+
+// Update visibility / menu of all messages of the current gist targets
+function markSelected() {
+    let links = document.querySelectorAll('a[target]');
+    let haystack = $gistTargets + ',';
+    for (let i = 0; links.length > i; i++) {
+        let target = links[i].getAttribute('target');
+        setTargetSelectionState(target, haystack.includes(target + ','));
+    }
+}
+
+// Show/hide system messages, sets global flag and cookie unless inhibited
 function visibilitySystemMessages(
   show = $showSystemMessages,
   global = true  // by default, set checkboxes + cookie as well
@@ -138,18 +300,21 @@ function visibilitySystemMessages(
     setVisibilityByName("special-system", $showSystemMessages)
 }
 
+// Show/hide commit messages, sets global flag and cookie
 function visibilityCommitMessages(hidden = $hideCommitMessages) {
     $hideCommitMessages = setCookie('CommitMessages', !!hidden);
     setCheckboxes('CommitMessages', $hideCommitMessages);
     setVisibilityByName("special-commit", !$hideCommitMessages)
 }
 
+// Show/hide Camelia output messages, sets global flag and cookie
 function visibilityCameliaOutput(hidden = $hideCameliaOutput) {
     $hideCameliaOutput = setCookie('CameliaOutput', !!hidden);
     setCheckboxes('CameliaOutput', $hideCameliaOutput);
     setVisibilityByName("special-camelia", !$hideCameliaOutput)
 }
 
+// Update input filter fields by ID and set associated cookie
 function updateFilter(name, value) {
     let inputs = document.querySelectorAll('input[id=' + name + ']');
     for (let i = 0; inputs.length > i; i++) {
@@ -158,6 +323,7 @@ function updateFilter(name, value) {
     return setCookie(name, value);
 }
 
+// Return true if any of the lowercase needles is found in the given haystack
 function containsAnyTexts(haystack, needles) {
     for (let i = 0; needles.length > i; i++) {
         let needle = needles[i].trim();
@@ -173,6 +339,7 @@ function containsAnyTexts(haystack, needles) {
     return false;
 }
 
+// Return true if lowercase needle is found in the given haystack
 function containsText(haystack, text) {
     let needle = text.trim();
     return needle
@@ -180,6 +347,7 @@ function containsText(haystack, text) {
       : true;
 }
 
+// Set visibility of messages depending on the filter settings
 function filterMessages(
   nick = $filterMessagesByNick,
   text = $filterMessagesByText
@@ -225,153 +393,4 @@ function filterMessages(
         visibilityCommitMessages();
         visibilityCameliaOutput();
     }
-}
-
-function filterMessagesByNick(nick) {
-    filterMessages(nick, $filterMessagesByText);
-}
-function filterMessagesByText(text) {
-    filterMessages($filterMessagesByNick, text);
-}
-
-function filterExcludeByNick(button) {
-    $filterExcludeByNick = setCookie(
-      'ExcludeByNick',
-      !!button.classList.contains('is-primary')
-    );
-    filterMessages();
-}
-function filterExcludeByText(button) {
-    $filterExcludeByText = setCookie(
-      'ExcludeByText',
-      !!button.classList.contains('is-primary')
-    );
-    filterMessages();
-}
-
-function setGistTargets(targets = $gistTargets) {
-    $gistTargets = setCookie('<.channel>Targets', targets);
-    document.getElementById('Gist').href =
-      '/<.channel>/gist.html?' + targets;
-}
-
-function elementOfTarget(element, target) {
-    return document.querySelector(element + '[target="' + target + '"]');
-}
-
-function clearGistTargets() {
-    setGistTargets("");
-    markSelected();
-}
-
-function addVisibleTargets() {
-    let newTargets = $gistTargets.split(',');;
-    let haystack   = $gistTargets + ',';
-    let trs = document.querySelectorAll('tr[target]');
-    for (let i = 0; trs.length > i; i++) {
-        let tr = trs[i];
-        if (!tr.classList.contains('is-hidden')) {
-            let target = tr.getAttribute('target');
-            if (!haystack.includes(target + ',')) {
-                setTargetSelectionState(target, true);
-                newTargets.push(target);
-            }
-        }
-    }
-    setGistTargets(newTargets.sort().join(','));
-}
-
-function removeVisibleTargets() {
-    let currentTargets = $gistTargets.split(",");
-    let newTargets = [];
-    for (let i = 0; currentTargets.length > i; i++) {
-        let target = currentTargets[i];
-        let tr     = elementOfTarget('tr', target);
-        if (tr) {
-            if (tr.classList.contains('is-hidden')) {
-                newTargets.push(target);
-            }
-            else {
-                setTargetSelectionState(target, false);
-            }
-        }
-    }
-    setGistTargets(newTargets.sort().join(','));
-}
-
-function setTargetSelectionState(target, on) {
-    let tr = elementOfTarget('tr', target);
-    if (tr) {
-        if (on) {
-            tr.classList.add('special-selected');
-        }
-        else {
-            tr.classList.remove('special-selected');
-        }
-    }
-    let option = elementOfTarget('a', target);
-    if (option) {
-        if (on) {
-            option.classList.add('is-hidden');
-            option.nextSibling.classList.remove('is-hidden');
-        }
-        else {
-            option.classList.remove('is-hidden');
-            option.nextSibling.classList.add('is-hidden');
-        }
-    }
-}
-
-function markSelected() {
-    let links = document.querySelectorAll('a[target]');
-    let haystack = $gistTargets + ',';
-    for (let i = 0; links.length > i; i++) {
-        let target = links[i].getAttribute('target');
-        setTargetSelectionState(target, haystack.includes(target + ','));
-    }
-}
-
-function removeTargetFromGist(link) {
-    let target = link.previousSibling.getAttribute('target');
-    let currentTargets = $gistTargets.split(',');
-    let newTargets = [];
-    let remove = false;
-    for (let i = 0; currentTargets.length > i; i++) {
-        let current = currentTargets[i];
-        if (current) {
-            if (current == target) {
-                remove = true;
-            }
-            else {
-                newTargets.push(current);
-            }
-        }
-    }
-    if (remove) {
-        setTargetSelectionState(target, false);
-    }
-    setGistTargets(newTargets.sort().join(','));
-}
-
-function addTargetToGist(link) {
-    let target = link.getAttribute('target');
-    let currentTargets = $gistTargets.split(',');
-    let newTargets = [];
-    let add = true;
-    for (let i = 0; currentTargets.length > i; i++) {
-        let current = currentTargets[i];
-        if (current) {
-            if (current == target) {
-                add = false;
-            }
-            else {
-                newTargets.push(current);
-            }
-        }
-    }
-    if (add) {
-        setTargetSelectionState(target, true);
-        newTargets.push(target);
-    }
-    setGistTargets(newTargets.sort().join(','));
 }
