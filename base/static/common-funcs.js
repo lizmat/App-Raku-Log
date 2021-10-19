6c79050f7a16cec3af4ec04b78c222fad8466a6a
ogdenwebb <ogdenwebb@gmail.com>
2021-10-19 17:18:13 +0300

    Pin container footer while sidebar toggle

--- base/static/common-funcs.js
+++ base/static/common-funcs.js
@@ -23,9 +23,11 @@ function setStateOfClassOnId(id, add, name = 'is-active') {
 // Init main position wrt sidebars
 if ($showLeftSide === true) {
     addClassToId('main-container', 'has-left-sidebar');
+    addClassToId('container-footer', 'has-left-sidebar');
 }
 if ($showRightSide === true) {
     addClassToId('main-container', 'has-right-sidebar');
+    addClassToId('container-footer', 'has-right-sidebar');
 }
 
 // targets for gist of current channel
@@ -133,9 +135,11 @@ function toggleLeftSidebar() {
     $showLeftSide = setCookie('showLeftSide', !$showLeftSide);
     if ($showLeftSide === "" || !$showLeftSide) {
         document.getElementById('main-container').classList.remove('has-left-sidebar');
+        document.getElementById('container-footer').classList.remove('has-left-sidebar');
         document.getElementById('navbar-left-toggle').querySelectorAll('svg')[1].classList.add('fa-chevron-right');
     } else {
         document.getElementById('main-container').classList.add('has-left-sidebar');
+        document.getElementById('container-footer').classList.add('has-left-sidebar');
         document.getElementById('navbar-left-toggle').querySelectorAll('svg')[1].classList.add('fa-chevron-left');
     }
     setDisplayById('left-column', $showLeftSide);
@@ -146,9 +150,11 @@ function toggleRightSidebar() {
     $showRightSide = setCookie('showRightSide', !$showRightSide);
     if ($showRightSide === "" || !$showRightSide) {
         document.getElementById('main-container').classList.remove('has-right-sidebar');
+        document.getElementById('container-footer').classList.remove('has-right-sidebar');
         document.getElementById('navbar-right-toggle').querySelectorAll('svg')[0].classList.add('fa-chevron-left');
     } else {
         document.getElementById('main-container').classList.add('has-right-sidebar');
+        document.getElementById('container-footer').classList.add('has-right-sidebar');
         document.getElementById('navbar-right-toggle').querySelectorAll('svg')[0].classList.add('fa-chevron-right');
     }
     setDisplayById('right-column', $showRightSide);
